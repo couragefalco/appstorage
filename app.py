e86fc156310c7ee927706e9b4fc72253fe45bff5
couragefalco <falco@havecourage.de>
2023-07-12 16:00:22 +0200

    1.0

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from azure.storage.blob import BlobServiceClient
 from apscheduler.schedulers.background import BackgroundScheduler
 import os
 from dotenv import load_dotenv
+from scipy.spatial.distance import directed_hausdorff
 
 load_dotenv()
 
@@ -32,32 +33,26 @@ def save_uploadedfile_api(uploadedfile):
 
 def preprocess_file(file_path):
     mesh = trimesh.load(file_path)
-    volume = mesh.volume
-    num_faces = mesh.faces.shape[0]
-    num_vertices = mesh.vertices.shape[0]
-    num_edges = mesh.edges.shape[0]
-    cog = mesh.center_mass
-    return volume, cog, num_faces, num_vertices, num_edges
+    point_cloud = mesh.sample(1000) # sample 1000 points from the mesh
+    return point_cloud
 
-def compare_files(volume, cog, num_faces, num_vertices, num_edges):
+def compare_files(uploaded_point_cloud):
     database = pd.read_csv('database.csv')
     database.columns = database.columns.str.strip()
-    top_matches = [[float("inf"), None, 0]] * 10  # Added third element for similarity score
-    max_distance = 0 # keep track of the maximum Euclidean distance
-    for index, row in database.iterrows():
-        distance = ((row['volume'] - volume)**2 + 
-                   (row['num_faces'] - num_faces)**2 + 
-                   (row['num_vertices'] - num_vertices)**2 + 
-                   (row['num_edges'] - num_edges)**2)**0.5
-        max_distance = max(distance, max_distance) # update the maximum distance if needed
+    grouped = database.groupby('filename') 
+    top_matches = [[float("inf"), {"filename": None}, 0]] * 10
+    max_distance = 0
+    for name, group in grouped:
+        stored_point_cloud = group[['point_cloud_x', 'point_cloud_y', 'point_cloud_z']].values
+        distance = max(directed_hausdorff(uploaded_point_cloud, stored_point_cloud)[0], directed_hausdorff(stored_point_cloud, uploaded_point_cloud)[0])
+        max_distance = max(distance, max_distance)
         for i in range(10):
             if distance < top_matches[i][0]:
-                # Insert distance, matched row, and placeholder for similarity score
-                top_matches.insert(i, [distance, row, 0])
+                top_matches.insert(i, [distance, {"filename": name}, 0])
                 top_matches.pop()
                 break
     for match in top_matches:
-        match[2] = 1 - match[0] / max_distance  # calculate normalized similarity score 
+        match[2] = 1 - match[0] / max_distance
     return top_matches
 
 def render_2d_projection(file_path, file_name):
@@ -84,14 +79,15 @@ def update_db():
     if os.path.exists("database.csv"):
         os.remove("database.csv")
     for filename in os.listdir("tempDir"):
-        volume, cog, num_faces, num_vertices, num_edges = preprocess_file(os.path.join("tempDir", filename))
-        data = {'filename': filename, 'volume': volume, 'cog_x': cog[0], 'cog_y': cog[1], 'cog_z': cog[2], 'num_faces': num_faces, 'num_vertices': num_vertices, 'num_edges': num_edges}
-        df = pd.DataFrame(data, index=[0])
+        point_cloud = preprocess_file(os.path.join("tempDir", filename))
+        data = {'filename': [filename]*len(point_cloud), 'point_cloud_x': point_cloud[:, 0], 'point_cloud_y': point_cloud[:, 1], 'point_cloud_z': point_cloud[:, 2]}
+        df = pd.DataFrame(data)
+        df.set_index(['filename', df.index], inplace=True)
         # check if the file exists, if not write header
         if not os.path.isfile('database.csv'):
-            df.to_csv('database.csv', mode='a', header=True, index=False)
+            df.to_csv('database.csv', mode='a', header=True)
         else: # else it exists so append without writing the header
-            df.to_csv('database.csv', mode='a', header=False, index=False)
+            df.to_csv('database.csv', mode='a', header=False)
     for filename in os.listdir("tempDir"):
         os.remove(os.path.join("tempDir", filename))
 
@@ -133,27 +129,18 @@ def main():
         
     if database_file is not None:
         save_uploadedfile(database_file)
-        volume, cog, num_faces, num_vertices, num_edges = preprocess_file(f'tempDir/{database_file.name}')        
-        # load or initialize data
-        if not os.path.isfile('database.csv'):
-            data = pd.DataFrame()
-        else:
-            data = pd.read_csv('database.csv')
-        # check if file name exists and only write new record
-        if database_file.name not in data['filename'].values:
-            blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
-            upload_file_to_blob(blob_service_client, f'tempDir/{database_file.name}', database_file.name, 'blobcontainer')
-            new_data = {'filename': database_file.name, 'volume:': volume, 'cog_x': cog[0], 'cog_y': cog[1], 'cog_z': cog[2], 'num_faces': num_faces, 'num_vertices': num_vertices, 'num_edges': num_edges}
-            df = pd.DataFrame(new_data, index=[0])
-            df.to_csv('database.csv', mode='a', header=False, index=False)
+        uploaded_point_cloud = preprocess_file(f'tempDir/{database_file.name}')        
+        # Process and upload the file here
         
     uploaded_file = st.file_uploader("Choose a file for comparison", type=['stl', 'step'], key='comparison_uploader')
     
     if uploaded_file is not None:
         save_uploadedfile(uploaded_file)
-        volume, cog, num_faces, num_vertices, num_edges = preprocess_file(f'tempDir/{uploaded_file.name}')
-        top_matches = compare_files(volume, cog, num_faces, num_vertices, num_edges)
-        st.write(f'Best Matches: {top_matches[0][1]["filename"]}, {top_matches[1][1]["filename"]}, {top_matches[2][1]["filename"]}') 
+        uploaded_point_cloud = preprocess_file(f'tempDir/{uploaded_file.name}')
+        top_matches = compare_files(uploaded_point_cloud)
+        print(top_matches[0])
+        print(top_matches[0][1])
+        st.write(f'Best Matches: {top_matches[0][1]["filename"]}, {top_matches[1][1]["filename"]}, {top_matches[2][1]["filename"]}')  
         st.write(f'Similarity Scores: {top_matches[0][2]}, {top_matches[1][2]}, {top_matches[2][2]}')
         # Preprocess and comparison code
         blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
@@ -164,7 +151,7 @@ def main():
             for blob in blobs_list:
                 print(blob.name)
             # Download the matched file from blob storage
-            download_blob(blob_service_client, 'blobcontainer', match[1]["filename"], 'tempdownload')
+            download_blob(blob_service_client, 'blobcontainer', str(match[1]["filename"]), 'tempdownload')
             render_2d_projection(f'tempdownload/{match[1]["filename"]}', match[1]["filename"])
         render_2d_projection(f'tempDir/{uploaded_file.name}', uploaded_file.name)
         # clear the tempdownload directory
@@ -172,3 +159,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
